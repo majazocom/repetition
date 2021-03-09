bee3ab33bdef71b5aba1a3e25f0a9b1d32466abd
majazocom <maja.gedda@zocom.se>
2021-03-09 13:27:28 +0100

    header and footer component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
+
+  const [headerTitle, setHeaderTitle] = useState('Plant app');
+  const [footerTitle, setFooterTitle] = useState('This is a footer');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={headerTitle} />
+      <Footer title={footerTitle} />
     </div>
   );
 }
